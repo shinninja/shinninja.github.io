d222f9a22ab9fe33f71719a981c59eb9ba8672c6
sinninja <dhye0513@gmail.com>
2020-03-06 09:10:36 +0900

    update ui.js

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,28 +1,39 @@
+$(function () {
+    NINJA.INIT();
+});
 
+var NINJA = {
+    UI: {
+         /**
+        * bottom nav service animation
+        *
+        * @author : 신다혜
+        */
+        animationBottomNav: function(){
+            var $bottomNav = $('.hm-bottom-nav');
+            var $service = $('.hm-bottom-nav__link--service');
+            var serviceTalk = 'hm-bottom-nav__link--service-talk';
+            var serviceVoice = 'hm-bottom-nav__link--service-voice';
 
- /**
-* bottom nav service animation
-*
-* @author : 신다혜
-*/
-animationBottomNav: function(){
-if (!$('.hm-bottom-nav').length) return;
+            if (!$bottomNav.length) return;
+            
+            function intervalClass(){
+                if ($service.hasClass(serviceTalk)) { //음성쇼핑
+                    $service.removeClass(serviceTalk).addClass(serviceVoice);
+                    $('.hm-bottom-nav__service-item--talk').fadeOut('fast');
+                    $('.hm-bottom-nav__service-item--voice').fadeIn('fast');
+                } else { //톡상담
+                    $service.removeClass(serviceVoice).addClass(serviceTalk);
+                    $('.hm-bottom-nav__service-item--voice').fadeOut('fast');
+                    $('.hm-bottom-nav__service-item--talk').fadeIn('fast');
+                }
+            }
 
-var $service = $('.hm-bottom-nav__link--service');
-var serviceTalk = 'hm-bottom-nav__link--service-talk';
-var serviceVoice = 'hm-bottom-nav__link--service-voice';
+            setInterval(intervalClass, 3500);
+        },
+    },
 
-function intervalClass(){
-    if ($service.hasClass(serviceTalk)) { //음성쇼핑
-        $service.removeClass(serviceTalk).addClass(serviceVoice);
-        $('.hm-bottom-nav__service-item--talk').fadeOut('fast');
-        $('.hm-bottom-nav__service-item--voice').fadeIn('fast');
-    } else { //톡상담
-        $service.removeClass(serviceVoice).addClass(serviceTalk);
-        $('.hm-bottom-nav__service-item--voice').fadeOut('fast');
-        $('.hm-bottom-nav__service-item--talk').fadeIn('fast');
+    INIT: function(){
+        NINJA.UI.animationBottomNav();
     }
 }
-
-setInterval(intervalClass, 3500);
-},
