af7590b693e6151e5b71b5a835fb6fdc4a1d7c73
sinninja <dhye0513@gmail.com>
2020-07-14 08:58:35 +0900

    create post function

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -5,30 +5,6 @@ $(function () {
 var NINJA = {
     UI: {
 
-        /* animationBottomNav */
-        animationBottomNav: function(){
-            var $bottomNav = $('.hm-bottom-nav');
-            var $service = $('.hm-bottom-nav__link--service');
-            var serviceTalk = 'hm-bottom-nav__link--service-talk';
-            var serviceVoice = 'hm-bottom-nav__link--service-voice';
-
-            if (!$bottomNav.length) return;
-            
-            function intervalClass(){
-                if ($service.hasClass(serviceTalk)) { //음성쇼핑
-                    $service.removeClass(serviceTalk).addClass(serviceVoice);
-                    $('.hm-bottom-nav__service-item--talk').fadeOut('fast');
-                    $('.hm-bottom-nav__service-item--voice').fadeIn('fast');
-                } else { //톡상담
-                    $service.removeClass(serviceVoice).addClass(serviceTalk);
-                    $('.hm-bottom-nav__service-item--voice').fadeOut('fast');
-                    $('.hm-bottom-nav__service-item--talk').fadeIn('fast');
-                }
-            }
-
-            setInterval(intervalClass, 3500);
-        },
-
         /* tabsType01 */
         tabsType01: function(target){
             var $target = $(target);
