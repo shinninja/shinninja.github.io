a403520addbc0411d185606e746f0cbfd4112c9d
sinninja <dhye0513@gmail.com>
2020-08-05 14:37:41 +0900

    update

--- nodeServer.js
+++ nodeServer.js
@@ -13,3 +13,24 @@ for(let i = 0; i < arrServer.length; i++) {
   app.use(express.static(__dirname + arrServer[i].path));
   app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 }
+
+
+//# install dependencies
+// npm install
+
+// # initialize
+// npm init
+
+// # install express
+// npm i express --save
+
+// # execute server
+// node himartServer.js
+
+// # 시작프로그램 등록
+// window + R
+// shell:startup
+
+
+// # Fiddler 패킷 가로채기
+// [AutoResponder] check Enable rules, Unmatched requests passthrough, Add Rule, Save
