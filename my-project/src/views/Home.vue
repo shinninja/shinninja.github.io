acd01f6baa13bebaf91bf09ac0177a31d0a74bc0
Oki-Boki <boki9518@gmail.com>
2021-01-05 21:09:09 +0900

    update vue of study

---
+++ my-project/src/views/Home.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <h1>Welcome to {{ title }}!</h1>
+        <input type="text" v-model="input1">
+        <button type="button" @click="getData">Get</button>
+        <button type="button" @click="setData">Set</button>
+
+        <select class="form-control" v-model="region" @change="changeRegion">
+            <option :key="i" :value="d.v" v-for="(d, i) in options">{{ d.t }}</option>
+        </select>
+
+        <table class="table table-bordered" v-show="tableShow"><!-- v-if="tableShow" -->
+            <tr :key="i" v-for="(d, i) in options">
+                <td>{{ d.v }}</td>
+                <td>{{ d.t }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            title: '개발자의 품격',
+            title2: 'Seoul',
+            input1: 'abc',
+            options: [
+                { v: 'S', t: 'Seoul' },
+                { v: 'J', t: 'Jeju' },
+                { v: 'B', t: 'Busan' },
+            ],
+            region: 'J',
+            tableShow: false
+        }
+    },
+    watch: { //특정데이터 모니터링
+        input1(){
+            console.log(this.input1);
+        }
+    },
+    methods: {
+        getData(){
+            alert(this.input1);
+        },
+        setData(){
+            this.input1 = '123345';
+        },
+        changeRegion(){
+            alert(this.region);
+        }
+    },
+    beforeCreate(){
+        console.log('beforeCreate');
+    },
+    created(){
+        console.log('created');
+    },
+    beforeMount(){
+        console.log('beforeMount');
+    },
+    mounted(){
+        console.log('mounted');
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate');
+    },
+    updated(){
+        console.log('updated');
+    },
+    beforeDestroy(){
+        console.log('beforeDestroy');
+    },  
+    destroyed(){
+        console.log('destroyed');
+    }
+}
+</script>
---
+++ my-project/src/views/ex/ex02_14.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="example">
+            <p>국가명 : <input type="text" placeholder="국가명"></p>
+            <table id="list">
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>국가명</th>
+                        <th>수도</th>
+                        <th>지역</th>
+                    </tr>
+                </thead>
+                <tbody id="contacts">
+                    
+                </tbody>
+            </table>
+        </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
